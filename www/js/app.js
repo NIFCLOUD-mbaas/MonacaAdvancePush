21565deb66387ef964803b32f140bfdcac83c5f8
ncmbadmin <dinh.thuy.duong@nifty.co.jp>
2016-07-22 18:22:00 +0900

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,382 @@
+var applicationID = "YOUR_APP_ID";
+var project_number = "PROJECT_NUMBER"; //アンドロイド端末のみ
+
+ // 【mBaaS】 APIキーの設定とSDKの初期化
+var appKey    = "YOUR_APP_KEY";
+var clientKey = "YOUR_CLIENT_KEY";
+var ncmb = new NCMB(appKey,clientKey);
+
+//公開ファイルURL
+var publicFileUrl = "https://mb.api.cloud.nifty.com/2013-09-01/applications/" + applicationID + "/publicFiles/";
+
+//現在の端末情報
+var currentInstallation;
+
+
+///// アプリが起動する際に実行されます
+$(function() {   
+    
+    // ボタン処理とメソッドをマッピングします
+    $("#LoginBtn").click(onLoginBtn);
+    $("#RegisterBtn").click(onRegisterBtn);
+    $("#SignupBtn").click(onSignupBtn);
+    $("#YesBtn_logout").click(onLogoutBtn);
+    $("#FavoriteBtn").click(onFavoriteBtn);
+    $("#UpdateFavoriteBtn").click(onUpdateFavoriteBtn);
+    
+    // 【mBaaS：プッシュ通知①】デバイストークンの取得し、サーバへ登録処理
+    document.addEventListener("deviceready", function(){
+            // デバイストークンを取得してinstallationに登録する
+            window.NCMB.monaca.setDeviceToken(
+                appKey,
+                clientKey,
+                project_number
+            );
+            
+            setTimeout(function(){
+                //currentInstallationの情報を取得
+                window.NCMB.monaca.getInstallationId(
+                    function(id) {
+                        //JavaScript SDKのInstallationクラスを利用して端末情報を取得
+                        ncmb.Installation.fetchById(id)
+                                         .then(function(installation){
+                                            //サーバから取得した結果をcurrentInstallationに保存
+                                            currentInstallation = installation;
+                                          })
+                                         .catch(function(err){
+                                            // エラー処理
+                                          });
+                    }
+                ); 
+        	},20000);
+            
+        },false);
+});  
+
+//----------------------------------会員管理-----------------------------------//
+// mBaaSから取得した「User」//現在ログイン中ユーザー情報データ格納用
+var currentLoginUser; 
+
+// mBaaSから取得した「Shop」クラスのデータ格納用
+var shopList;
+
+// mBaaSから取得した「Shop」情報データ格納用（取得しやすくため配列形変更済み）
+var shopArray;
+
+//現在詳細ページを表示するお店
+var currentShopId; 
+
+/* --------- 【mBaaS：会員管理③】ユーザー情報更新----------- */
+
+function onRegisterBtn()
+{
+    //入力フォームからnickname, prefecture, genderを値セット
+    var nickname = $("#reg_nickname").val();
+    var prefecture = $("#reg_prefecture").val();
+    var gender = $('input[name=reg_gender]:checked').val();
+    
+    // currentLoginUserユーザー情報を設定
+    currentLoginUser.nickname = nickname;
+    currentLoginUser.prefecture = prefecture;
+    currentLoginUser.gender = gender;
+    currentLoginUser.favorite = [];
+    
+    // user情報の更新
+    currentLoginUser.update()
+                    .then(function(user) {
+                            // 更新成功時の処理
+                            /// 【mBaaS：プッシュ通知③】installationにユーザー情報を紐づける
+                            if (currentInstallation) {
+                                // ユーザー情報を設定
+                                currentInstallation.prefecture = prefecture;
+                                currentInstallation.gender = gender;
+                                currentInstallation.favorite = [];
+                                
+                                // installation情報の更新
+                                currentInstallation.update()
+                                    .then(function(installation) {
+                                        // installation更新成功時の処理
+                                        alert("会員情報登録に成功");
+                                        //お店一覧画面遷移
+                                        showShopList();
+                                    })
+                                    .catch(function(error) {
+                                        // installation更新失敗時の処理
+                                    });
+                            }
+                    })
+                    .catch(function(error) {
+                        // 更新失敗時の処理
+                        alert("会員情報登録に失敗！次のエラー発生：" + error);
+                    });
+            
+}
+
+/* --------- 【mBaaS：会員管理①】会員登録用メールを要求する ------------ */
+function onSignupBtn() {
+    var mailaddress = $("#signup_mailaddress").val();
+    ncmb.User.requestSignUpEmail(mailaddress)
+         .then(function(data){
+            alert("リクエストを送信しました！メールをご確認ください。");
+            // 送信後処理
+            $.mobile.changePage('#LoginPage');
+         })
+         .catch(function(err){
+            alert("リクエスト失敗！次のエラー発生: " + err);
+           // エラー処理
+           $.mobile.changePage('#LoginPage');
+         });
+}
+
+/* --------- 【mBaaS：会員管理②】メールアドレスとパスワードでログイン ----------- */
+
+function onLoginBtn()
+{
+    // 入力したメールアドレスの値
+    var mailaddress = $("#login_mailaddress").val();
+    // 入力したパスワードの値
+    var password = $("#login_password").val();
+    
+    // メールアドレスとパスワードでログイン処理実施
+    ncmb.User.loginWithMailAddress(mailaddress, password)
+        .then(function(user) {
+            // ログイン成功時の処理
+            alert("ログイン成功");
+            currentLoginUser = ncmb.User.getCurrentUser();
+            if(currentLoginUser.nickname) {
+                showShopList();
+            } else {
+                // ユーザ情報登録画面遷移
+                $.mobile.changePage('#RegisterPage');
+            }
+        })
+        .catch(function(error) {
+            // ログイン失敗時の処理
+            alert("ログイン失敗！次のエラー発生: " + error);
+        });
+}
+
+function onLogoutBtn()
+{
+    //ログアウト処理実施
+    ncmb.User.logout();
+
+    //ログイン中ユーザをリセット
+    currentLoginUser = null;
+    
+    //処理結果を表示
+    alert('ログアウト成功');
+    
+    //ログインページに移動
+    $.mobile.changePage('#LoginPage');
+}
+
+
+// 【mBaaS：ファイルストア②】Shop画像の取得
+// 取得した「Shop」クラスデータからShop画面用の画像名を取得
+function showShopDetail(shopId) {
+    //選択中のお店詳細
+    currentShopId = shopId;
+    var shopTmp = shopList[shopId]; 
+    $("#shopName").text(shopTmp.name);
+    $("#shopImage").attr("src" , publicFileUrl + shopTmp.sale_image); 
+    
+    // お気に入り登録されている場合の表示設定
+    if (currentLoginUser.favorite.indexOf(shopId) >= 0) {
+        
+        //お気に入り登録された場合の♥︎表示
+        $("#favoriteImage").attr("src" , "images/favorite.png");
+        //お気に入り登録ボタンを非アクティブ表示
+        $("#FavoriteBtn").addClass('ui-disabled');
+        
+    }else{
+        
+        //お気に入り登録された場合の♡表示
+        $("#favoriteImage").attr("src" , "images/nofavorite.png");
+        //お気に入り登録ボタンをアクティブ表示
+        $("#FavoriteBtn").removeClass('ui-disabled');
+    
+    }
+    //ShopPageを表示します
+    $.mobile.changePage('#ShopPage');
+}
+
+
+//　mBaaSに登録されているShop情報を取得してリストに表示する
+function showShopList() {
+    
+    //リストをリセットします。
+    $('#listShop').empty();
+    
+    //表示名を指定します。
+    $("#nickName").text(currentLoginUser.nickname);
+    
+    // 【mBaaS：データストア】「Shop」クラスのデータを取得
+　　// 「Shop」クラスのクエリを作成
+    var ShopClass = ncmb.DataStore("Shop");
+    
+    //  データストアを検索
+    ShopClass.fetchAll()
+        .then(function(shops) {
+    
+    　　　　// 検索成功時の処理
+            shopList = convertShopList(shops);
+            
+            // shopArrayに取得しやすいショップのobjectIdと「Shop」クラスの情報を保持
+            shopArray = shops;
+            
+            // listShopの内容を設定
+            for (var i = 0; i < shops.length; i++) {
+                var shop = shops[i];
+                var tmpStr = '<li class="ui-li-has-thumb"><a id="ShopBtn" href="#" onclick="showShopDetail(\'' + shop.objectId + '\');"><img src="'+ publicFileUrl + shop.icon_image +'" />'
+            + shop.name + '</a></li>';
+                $("#listShop").append(tmpStr);
+            }
+            
+            // listShopの内容を更新
+            $('#listShop').listview('refresh');
+        })
+        .catch(function(error) {
+            // 検索失敗時の処理
+            alert(error.message);
+    }); 
+    $.mobile.changePage('#TopPage');
+}
+
+
+//　mBaaSにお気に入り登録されているShop情報を取得してリストに表示する
+function showFavorite() {
+
+　　//リストをリセットします。
+    $("#listFavoriteShop").empty();
+    
+    //表示名を指定します。
+    $("#favorite_nickName").text(currentLoginUser.nickname + "のお気に入りショップ");
+    
+    //お気に入り登録した値を取得
+    var fav_shops = currentLoginUser.favorite;
+    
+    //ショップ一覧を表示
+    for (var i = 0; i < shopArray.length; i++) {
+        var shop = shopArray[i]; 
+        var selectStr = "selected='true'";
+        
+        //ショップのお気に入りが登録された場合の表示
+        var tmpStrOff = "<li data-role='fieldcontain'><label for='" + shop.objectId + "'>" + shop.name + "</label><select name='favorite_shop' id='" + shop.objectId + "' data-role='slider' data-theme='e' ><option value='off' " + selectStr+ ">Off</option><option value='" + shop.objectId + "'>On</option></select></li> ";
+        
+        //ショップのお気に入りが登録されていない場合の表示
+        var tmpStrOn = "<li data-role='fieldcontain'><label for='" + shop.objectId + "'>" + shop.name + "</label><select name='favorite_shop' id='" + shop.objectId + "' data-role='slider' data-theme='e' ><option value='off'>Off</option><option value='" + shop.objectId + "' " + selectStr+ " >On</option></select></li> ";
+        
+        //var tmpStrOff = "<li><label for='" + shop.objectId + "'>" + shop.name + "</label><select name='flipFavorite' id='" + shop.objectId + "' data-role='slider' data-theme='e' ><option value='off' " + selectStr+ ">Off</option><option value='" + shop.objectId + "'>On</option></select></li> ";
+        //var tmpStrOn = "<li><label for='" + shop.objectId + "'>" + shop.name + "</label><select name='flipFavorite' id='" + shop.objectId + "' data-role='slider' data-theme='e' ><option value='off'>Off</option><option value='" + shop.objectId + "' " + selectStr+ " >On</option></select></li> ";
+        
+        if ($.inArray(shop.objectId, fav_shops) == -1 ){ 
+            $("#listFavoriteShop").append(tmpStrOff);
+        }else{
+            $("#listFavoriteShop").append(tmpStrOn);
+        }   
+    }
+
+    setTimeout(function(){
+    	 $('#listFavoriteShop').listview('refresh');
+         $('select[name=favorite_shop]').slider('refresh'); 
+	},1000);
+
+    //画面遷移
+    $.mobile.changePage('#FavoritePage');
+}
+
+
+function onUpdateFavoriteBtn() {
+    //Get ON list
+    var array = [];
+    $('[name=favorite_shop]').each(function() {
+        var tmp = $(this).val();
+        if (tmp != "off") {
+            array.push(tmp);
+        }
+    });
+    // 【mBaaS：会員管理④】ユーザー情報の更新
+    
+    // ログイン中のユーザーの更新favoriteを設定
+    currentLoginUser.favorite = array;
+    // ユーザー情報を更新
+    currentLoginUser.update()
+                    .then(function(currentLoginUser) {
+                        // 更新に成功した場合の処理
+                        if (currentInstallation) {
+                            
+                            // 【mBaaS：プッシュ通知②】installationにユーザー情報を紐づける
+                            // お気に入り情報を設定
+                            currentInstallation.favorite = array;
+                            
+                            // installation情報の更新
+                            currentInstallation.update()
+                                .then(function(installation) {
+                                    // installation更新成功時の処理
+                                    alert("お気に入り情報更新に成功しました");
+                                    showFavorite();
+                                })
+                                .catch(function(error) {
+                                    // installation更新失敗時の処理
+                                });                    
+                        }
+                    })
+                    .catch(function(error) {
+                        // 更新に失敗した場合の処理
+                        alert("お気に入り登録に失敗！次のエラー発生：" + error);
+                    });
+}
+
+// 「お気に入り登録」ボタン押下時の処理
+function onFavoriteBtn() {
+    // 【mBaaS：会員管理④】ユーザー情報の更新
+    
+　　// ログイン中のユーザーのお気に入り情報（favorite）を設定
+    currentLoginUser.favorite.push(currentShopId);
+    
+    // ユーザー情報を更新
+    currentLoginUser.update()
+                    .then(function(currentLoginUser) {
+                            // 更新に成功した場合の処理
+                            
+                            // 【mBaaS：プッシュ通知②】installationにユーザー情報を紐づける
+                            if(currentInstallation ){
+                                // お気に入り情報を設定
+                                currentInstallation.favorite = currentLoginUser.favorite;
+                                // installation情報の更新
+                                currentInstallation.update()
+                                    .then(function(installation) {
+                                        // 更新に成功した場合の処理
+                                        alert("お気に入り登録に成功");
+                                        showShopDetail(currentShopId);
+                                    })
+                                    .catch(function(error) {
+                                        // 更新に失敗した場合の処理
+                                    });   
+                            }
+                    })
+                    .catch(function(error) {
+                        // 更新に失敗した場合の処理
+                        alert("お気に入り登録に失敗！次のエラー発生：" + error);
+                    });
+}
+
+function showInfoPage() {
+    // 各ラベルに値を設定
+    $("#info_nickName").text(currentLoginUser.nickname);
+    $("#info_gender").text(currentLoginUser.gender);
+    $("#info_prefecture").text(currentLoginUser.prefecture);
+    $("#info_mailaddress").text(currentLoginUser.mailAddress);
+    
+    $.mobile.changePage('#InfoPage');
+}
+
+function convertShopList (shops) {
+    var shopListTmp = {};
+    for (var i = 0; i < shops.length; i++) {
+        var shop = shops[i];
+        shopListTmp[shop.objectId] = shop;
+    }
+    return shopListTmp;
+}
